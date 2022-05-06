86b31b88d37451af6755d8611c402aef1af70b2e
User <User@JRDOTNETFSECO-3>
2022-05-06 23:43:09 +0530

    Add project files.

---
+++ E-Auction/Controllers/BuyerController.cs
@@ -0,0 +1,34 @@
+﻿using BuyerAPI.Service.Contract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Auction
+{    
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/{v:apiVersion}/seller")]
+    public class BuyerController : ControllerBase
+    {
+        #region Declarations
+        private readonly ILogger<BuyerController> _logger;
+        private readonly IBuyerService _buyerService;
+        #endregion
+
+        #region Constructor
+        public BuyerController(ILogger<BuyerController> logger, IBuyerService buyerService)
+        {
+            _logger = logger;
+            _buyerService = buyerService;
+        }
+        #endregion
+
+        #region Operations
+
+        #endregion
+    }
+}
